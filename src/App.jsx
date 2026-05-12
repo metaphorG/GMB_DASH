ed04f1cda8957b0cfd389eeedc5b0cb9a344e142
metaphorG <104073961+metaphorG@users.noreply.github.com>
2026-05-12 23:17:59 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { useState, useMemo, useCallback, Fragment, useEffect, useRef } from "rea
 // ═══════════════════════════════════════════════════════════════════
 // JSONBIN CONFIGURATION — paste your values here
 // ═══════════════════════════════════════════════════════════════════
-const JSONBIN_ID  = '6a035267c0954111d81268ad';      // e.g. '6830abc123def456'
+const JSONBIN_ID  = '6a0367b8250b1311c33de7a7';      // e.g. '6830abc123def456'
 const JSONBIN_KEY = '$2a$10$F1LfGWg51BWexG/3aptWR.9LiWTSjKP5ozoASlZydrByLgYsa5wSy';  // e.g. '$2b$10$abc...'
 const BIN_URL     = 'https://api.jsonbin.io/v3/b/' + JSONBIN_ID;
 const HDR_READ    = { 'X-Master-Key': JSONBIN_KEY, 'X-Bin-Meta': 'false' };
